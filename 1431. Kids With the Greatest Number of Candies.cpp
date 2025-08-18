09f72915836d15a7a505fdceb0e72b52079073fc
serkanyilmaz-ce <serkan_yilmaz@zoho.com>
2025-08-18 22:55:16 +0000

    Add solution and problem description for "1431. Kids With the Greatest Number of Candies"

---
+++ 1431. Kids With the Greatest Number of Candies.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies)
+    {
+        // Find the maximum number of candies any kid currently has
+        int _maxElement = *max_element(candies.begin(), candies.end());
+        vector<bool> result;
+        // Check if each kid can have the greatest number of candies after receiving extra candies
+        for (int i = 0; i < candies.size(); i++)
+            result.push_back(candies[i] + extraCandies >= _maxElement);
+        return result;
+    }
+};
